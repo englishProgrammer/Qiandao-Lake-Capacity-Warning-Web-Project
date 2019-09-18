d6cb18c74bb829d5a485d0da380e7d0f29a72ffe
jeffrey <409767510@qq.com>
2019-09-18 19:51:01 +0800

    Update other

--- mainproject/capacityWeb/models.py
+++ mainproject/capacityWeb/models.py
@@ -4,10 +4,12 @@ from django.db import models
 # Create your models here.
 
 class Camera(models.Model):
-    camid = models.IntegerField(db_column='camId', primary_key=True)  # Field name made lowercase.
-    scenicid = models.IntegerField(db_column='scenicId')  # Field name made lowercase.
+    camid = models.CharField(db_column='camId', primary_key=True, max_length=11)  # Field name made lowercase.
+    scenicid = models.ForeignKey('Scenic', models.DO_NOTHING, db_column='scenicId')  # Field name made lowercase.
     camplace = models.CharField(db_column='camPlace', max_length=255, blank=True,
                                 null=True)  # Field name made lowercase.
+    camlgn = models.FloatField(db_column='camLgn', blank=True, null=True)  # Field name made lowercase.
+    camlat = models.FloatField(db_column='camLat', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
         managed = False
@@ -16,8 +18,9 @@ class Camera(models.Model):
 
 
 class Recordnums(models.Model):
-    scenicid = models.IntegerField(db_column='scenicId')  # Field name made lowercase.
-    camid = models.IntegerField(db_column='camId', blank=True, null=True)  # Field name made lowercase.
+    scenicid = models.ForeignKey('Scenic', models.DO_NOTHING, db_column='scenicId')  # Field name made lowercase.
+    camid = models.ForeignKey(Camera, models.DO_NOTHING, db_column='camId', blank=True,
+                              null=True)  # Field name made lowercase.
     nums = models.IntegerField(blank=True, null=True)
     year = models.IntegerField()
     month = models.IntegerField(blank=True, null=True)
@@ -35,8 +38,11 @@ class Recordnums(models.Model):
 
 class Recordwarnings(models.Model):
     warningid = models.AutoField(db_column='warningId', primary_key=True)  # Field name made lowercase.
-    scenicid = models.IntegerField(db_column='scenicId', blank=True, null=True)  # Field name made lowercase.
+    scenicid = models.CharField(db_column='scenicId', max_length=11, blank=True,
+                                null=True)  # Field name made lowercase.
+    camid = models.CharField(db_column='camId', max_length=11, blank=True, null=True)  # Field name made lowercase.
     level = models.IntegerField(blank=True, null=True)
+    type = models.IntegerField(blank=True, null=True)
     createat = models.CharField(db_column='createAt', max_length=255, blank=True,
                                 null=True)  # Field name made lowercase.
 
@@ -46,7 +52,7 @@ class Recordwarnings(models.Model):
 
 
 class Scenic(models.Model):
-    scenicid = models.IntegerField(db_column='scenicId', primary_key=True)  # Field name made lowercase.
+    scenicid = models.CharField(db_column='scenicId', primary_key=True, max_length=11)  # Field name made lowercase.
     scenicname = models.CharField(db_column='scenicName', max_length=255, blank=True,
                                   null=True)  # Field name made lowercase.
     warning1nums = models.IntegerField(db_column='warning1Nums', blank=True, null=True)  # Field name made lowercase.
